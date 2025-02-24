ec67ce4150a134318b6d5dec62e060ff1722c3d6
ClorindaMiranda <cmiranda160709@gmail.com>
2025-02-24 15:53:14 -0600

    Add new ag-grid into project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { AgGridAngular} from 'ag-grid-angular';
+import { ColDef} from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [AgGridAngular],
+  template: 
+  `
+    <!-- The AG Grid component -->
+    <ag-grid-angular
+    class="ag-theme-quartz" style='height:500px;width:100%'
+    [rowData]="rowData" [columnDefs]="colDefs"/>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+];
+
+// Column Definitions: Defines the columns to be displayed.
+colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true })]
