a641ca655d34723888cea1af7f8662c0dc5e74b3
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2025-02-25 02:03:09 -1200

    downgrade the ag-grid version and commented the registry in app.config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,49 @@
 import { Component } from '@angular/core';
-import { AgGridAngular} from 'ag-grid-angular';
-import { ColDef} from 'ag-grid-community';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-quartz.css';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+// import 'ag-grid-community/styles/ag-grid.css';
+// import 'ag-grid-community/styles/ag-theme-quartz.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [AgGridAngular],
-  template: 
-  `
+  template: `
     <!-- The AG Grid component -->
-    <ag-grid-angular
-    class="ag-theme-quartz" style='height:500px;width:100%'
-    [rowData]="rowData" [columnDefs]="colDefs"/>
-`,
-  styleUrl: './app.component.css'
+    <div class="row">
+      <div class="col-12">
+        <!-- The AG Grid component -->
+        <ag-grid-angular
+          style="height: 500px;width:100%"
+          [rowData]="rowData"
+          [columnDefs]="colDefs"
+          [defaultColDef]="defaultColDef"
+          class="ag-theme-quartz"
+        >
+          <!-- Apply the AG Grid theme -->
+        </ag-grid-angular>
+      </div>
+    </div>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-];
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
 
-// Column Definitions: Defines the columns to be displayed.
-colDefs: ColDef[] = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-];
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
+
+  defaultColDef = {
+    flex: 1,
+    minWidth: 100,
+  };
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
+// import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true })],
 };
